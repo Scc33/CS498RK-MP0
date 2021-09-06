24ed0ed41bf6f54180c15c9f37bf4e510a36f2bf
Sean Coughlin <seanmc4@illinois.edu>
2021-09-05 22:16:11 -0500

    Tidy up

--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-//Help from
-//https://www.tutorialrepublic.com/faq/how-to-change-the-background-color-of-a-web-page-using-javascript.php
-function changeBodyBg(color){
-    document.body.style.background = color;
-}
