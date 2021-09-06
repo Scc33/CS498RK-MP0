da0f7a89f83471d90e012b22b5901b9379c38818
Sean Coughlin <seanmc4@illinois.edu>
2021-09-05 22:11:10 -0500

    Mess around with JS and finish requirements

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,5 @@
-/* Your JS here. */
-console.log('Hello World!')
+//Help from
+//https://www.tutorialrepublic.com/faq/how-to-change-the-background-color-of-a-web-page-using-javascript.php
+function changeBodyBg(color){
+    document.body.style.background = color;
+}
